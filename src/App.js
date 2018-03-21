5b027c6cce6547122f6a4320ae6c5d8191475f6c
adkahane <adam.kahane.nj@gmail.com>
2018-03-21 01:18:55 -0700

    cleaning up/favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import DolphinCard from "./components/DolphinCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
 import dolphins from "./dolphins.json";
 import "./App.css";
 
